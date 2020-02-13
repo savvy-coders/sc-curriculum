00be705faab9cb42371146232438594045e4530d
ambientstl <41842065+ambientstl@users.noreply.github.com>
2020-02-13 15:35:49 -0600

    add comments/instructions to activity 3.2.7

--- Week3-JavaScript/3.2-JSFunctions/3.2-Activities/7-Evr-Callbacks.js
+++ Week3-JavaScript/3.2-JSFunctions/3.2-Activities/7-Evr-Callbacks.js
@@ -1,3 +1,4 @@
+// "regular" functions
 function greet(name) {
   return `Hello ${name}!`;
 }
@@ -10,7 +11,11 @@ function informAboutSale(name) {
   return `${name}, we're having a sale!`;
 }
 
+// function that takes a callback function as an argument
 function getNameAndShowMessage(callback) {
   const name = prompt("Please Enter Your Name: ");
   alert(callback(name));
 }
+
+// pass each "regular" function into getAndShowMessage as a callback function
+
