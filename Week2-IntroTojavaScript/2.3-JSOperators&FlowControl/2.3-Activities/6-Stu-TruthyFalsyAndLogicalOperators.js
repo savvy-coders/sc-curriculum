6accccb32d193b7095b5795f49a1dd7836c1f2a3
ambientstl <41842065+ambientstl@users.noreply.github.com>
2020-06-24 11:19:25 -0500

    update lesson 2.3 to include ternary operator

--- Week2-IntroTojavaScript/2.3-JSOperators&FlowControl/2.3-Activities/6-Stu-TruthyFalsyAndLogicalOperators.js
+++ Week2-IntroTojavaScript/2.3-JSOperators&FlowControl/2.3-Activities/6-Stu-TruthyFalsyAndLogicalOperators.js
@@ -14,6 +14,9 @@ console.log(!``);
 console.log(!(1 && "false"));
 console.log(NaN || !"true");
 
+// ? TO DO: set the age variable equal to a number, then use the conditional operator to construct an expression that returns "full menu" if the age is over 10, and "kids menu" if the age is less than 10
+let age;
+
 // ? TO DO: using only logical operators (no comparison operators), write an if statement that prints "The number is zero" when num is 0.
 let num = 0;
 // * your if statement here *
