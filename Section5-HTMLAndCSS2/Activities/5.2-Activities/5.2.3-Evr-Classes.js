c3498143c797c8a9ed637a817fbe2a40474ab459
Cameron Brantley <brantley.cameron@gmail.com>
2021-03-28 15:41:41 -0500

    Markdown File Update

--- Section5-HTMLAndCSS2/Activities/5.2-Activities/5.2.3-Evr-Classes.js
+++
@@ -1,6 +0,0 @@
-// refactor your Animal function constructor and methods to use the class keyword
-
-// extend the Animal class with a type of animal
-// add some unique methods and properties to the class extension
-
-// create an instance of the class extension and call one of its methods
