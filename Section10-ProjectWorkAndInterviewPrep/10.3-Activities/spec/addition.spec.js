10424ea9c842002262403a3ad182c20a8dbbe1de
Johan Bester <johan.bester@yahoo.com>
2021-06-13 15:47:55 -0500

    Update Jasmin Testing Curriculum

---
+++ Section10-ProjectWorkAndInterviewPrep/10.3-Activities/spec/addition.spec.js
@@ -0,0 +1,18 @@
+
+// const assert = require('assert');
+// const addition = require('../addition.js');
+
+// describe("addition()", function() {
+//     //act
+//     let a = 2;
+//     let b = 2;
+//     let sum = 4;
+
+//     //arrange
+//     let result = addition(a,b);
+
+//     //assert
+//     it("it should return 4", function(){
+//       expect(result).toEqual(sum);
+//     });
+//   });
---
+++ Section10-ProjectWorkAndInterviewPrep/10.3-Activities/spec/helloWorld.spec.js
@@ -0,0 +1,15 @@
+// const assert = require('assert');
+// const helloWorld = require("../helloWorld");
+
+// describe("helloWorld()", function() {
+//   //act
+//   let greeting = 'Hello World';
+
+//   //arrange
+//   let result = helloWorld;
+
+//   //assert
+//   it("it should return - Hello World", function(){
+//     expect(result).toEqual(greeting);
+//   });
+// });
